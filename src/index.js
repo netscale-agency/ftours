f351c5c30edd7e12f5b3c9909686bc048f7ecf2e
ivicaNetscale <ivica.zlatar@netscale.agency>
2022-04-20 20:08:52 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Routes>
-<Route path='/' element={<App />} />
+  <Route path='/' element={<App />} />
   </Routes>
   </BrowserRouter>,
 );
