aabc9a525f9a03af0b866da3a7c03955634494fd
lovre jerkunica <lovre.jerkunica@netscale.agency>
2022-04-28 16:11:41 +0200

    fixed-footer-alignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './global-styles'
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
