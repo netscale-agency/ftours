db70c118d735dc736c88f2b5a440347406d17873
lovre jerkunica <lovre.jerkunica@netscale.agency>
2022-04-29 20:39:29 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Carousel } from 'bootstrap';
 import React from 'react';
 import './App.css';
+import CarouselContainer from './components/slider/CarouselContainer';
 import { FooterContainer } from './container/footer'
 
 function App() {
   return (
     <>
+      <CarouselContainer />
       <FooterContainer />
     </>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { GlobalStyles } from './global-styles'
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
