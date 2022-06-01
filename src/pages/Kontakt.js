3bfe23248f96a1fc3a59db0f7df207df152ae520
Ogi <ognjen1802@gmail.com>
2022-06-01 16:53:05 +0200

    finished contact page

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -9,8 +9,8 @@ function Kontakt() {
         <div className="container-kontakt-purple">Kontakt</div>
       </section>
       <Container className="containter-kontakt">
-        <Row>
-          <Col>
+        <Row className="row-kontakt-page">
+          <Col className="col-kontakt">
             <Accordion defaultActiveKey={["0"]}>
               <Accordion.Item eventKey="1">
                 <Accordion.Header>Radno vrijeme</Accordion.Header>
@@ -47,28 +47,57 @@ function Kontakt() {
               <Accordion.Item eventKey="2">
                 <Accordion.Header>Kontakt</Accordion.Header>
                 <Accordion.Body>
-                  Adresa: Trg Hrvatske bratske zajednice 2, 21000 Split
-                  <br />
+                  <p className="p-kontakt">
+                    <span className="span-kontakt">• Adresa: </span>
+                    Trg Hrvatske bratske zajednice 2, 21000 Split
+                    <br />
+                    <span className="span-kontakt">• Telefon: </span>
+                    <a href="tel:+38521344842" className="a-kontakt-link">
+                      021 344 842
+                    </a>
+                    <br />
+                    <span className="span-kontakt">• Fax: </span>
+                    <a href="tel:+38521339609" className="a-kontakt-link">
+                      021 339 609
+                    </a>
+                    <br />
+                    <span className="span-kontakt">• Email: </span>
+                    <a href="mailto:desk@f-tours.hr" className="a-kontakt-link">
+                      desk@f-tours.hr
+                    </a>
+                    <br />
+                  </p>
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="3">
                 <Accordion.Header>Informacije</Accordion.Header>
                 <Accordion.Body>
-                  tel: 021 344 842
-                  <br />
-                  fax: 021 339 609
-                  <br />
-                  <a href="mailto:desk@f-tours.hr">desk@f-tours.hr</a>
+                  <p className="p-kontakt">
+                    <span className="span-kontakt">• OIB: </span>
+                    05871616331
+                    <br />
+                    <span className="span-kontakt">• IBAN: </span>
+                    HR07 2390 0011 1012 6276 4<br />
+                    kod HRVATSKA POŠTANSKA BANKA d.d.
+                    <br />
+                    <span className="span-kontakt">• SWIFT/BIC: </span>
+                    HPBZHR2X
+                    <br />
+                    <span className="span-kontakt">• IDK: </span>
+                    HR-AB-21-060333584
+                    <br />
+                  </p>
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
           </Col>
-          <Col>
+          <Col className="col-gmap">
             <iframe
+              className="gmap"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2893.3958355951513!2d16.43769951522363!3d43.51493766937882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13355e0079b9df27%3A0x7b611de1e41c78fa!2sF-TOURS%20Ltd.!5e0!3m2!1sen!2shr!4v1653993097991!5m2!1sen!2shr"
-              width="600"
-              height="350"
+              frameborder="0"
               style={{ border: "0" }}
+              title="F-tours map location"
               allowfullscreen=""
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
